b91f21e3879f783a8c7ca341f27ec7d0dc58d176
sarath280 <myappleid2849@gmail.com>
2014-12-17 22:00:48 +0530

    Update and rename cm.mk to aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_condor
+PRODUCT_NAME := aokp_condor
 PRODUCT_RELEASE_NAME := MOTO E
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
